f89322a96ea52cd90126769cc81bf36fce566e33
samuelhero <samherox@gmail.com>
2023-02-16 14:53:46 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include stdio.h
 int main(void) 
 {
-	printf("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 		return 0;
 }
