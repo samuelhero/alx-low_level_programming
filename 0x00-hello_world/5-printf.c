2e3f8e35dc5df030263d3e0dd8694e6af2acb824
samuelhero <samherox@gmail.com>
2023-02-16 15:20:24 -0800

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stio.h>
+/**
+ * main - print a line of code using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
